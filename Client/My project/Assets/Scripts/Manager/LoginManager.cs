cfdd6ec9329662cb456694be93b9df54829d8150
Sean-mn <b01080080329@gmail.com>
2025-05-12 20:36:51 +0900

    Login 구현

--- Client/My project/Assets/Scripts/Manager/LoginManager.cs
+++ Client/My project/Assets/Scripts/Manager/LoginManager.cs
@@ -1,18 +1,65 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LoginManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{  
+    public static LoginManager Instance { get; private set; }
+
+    private string _loginUrl = "http://localhost:5000/api/auth/login";
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TryLogin(string username, string password)
     {
-        
+        StartCoroutine(LoginCor(username, password));
+    }
+
+    private IEnumerator LoginCor(string username, string password)
+    {
+        var loginData = new LoginRequest { Username = username, Password = password };
+        string jsonData = JsonUtility.ToJson(loginData);
+
+        UnityWebRequest req = new UnityWebRequest(_loginUrl, "POST");
+        byte[] body = Encoding.UTF8.GetBytes(jsonData);
+
+        req.uploadHandler = new UploadHandlerRaw(body);
+        req.downloadHandler = new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+        yield return req.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("로그인 성공: " + request.downloadHandler.text);
+
+            TokenResponse tokenResponse = JsonUtility.FromJson<TokenResponse>(request.downloadHandler.text);
+            PlayerPrefs.SetString("JWT_TOKEN", tokenResponse.token);
+        }
+        else
+        {
+            Debug.LogError("로그인 실패: " + request.error);
+        }
+    }
+
+    [System.Serializable]
+    private class LoginRequest
+    {
+        public string Username;
+        public string Password;
+    }
+
+    [System.Serializable]
+    private class TokenResponse
+    {
+        public string token;
     }
 }
---
+++ Client/My project/Assets/Scripts/UI/UI_Login.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Login : MonoBehaviour
+{
+    [SerializeField] private InputField _userNameInput;
+    [SerializeField] private InputField _passwordInput;
+    [SerializeField] private Button _loginButton;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnLogined()
+    {
+        string username = _userNameInput.text;
+        string password = _passwordInput.text;
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            Debug.Log("아이디와 비밀번호를 입력해주세요.");
+            return;
+        }
+
+
+    }
+}
