4056e565728a7c24e9d94bbd499a591d1fcb8520
Sean-mn <b01080080329@gmail.com>
2025-05-13 17:35:04 +0900

    수정

--- Server/JWT_Practice/JWT_Practice/obj/Debug/net8.0/JWT_Practice.AssemblyInfo.cs
+++ Server/JWT_Practice/JWT_Practice/obj/Debug/net8.0/JWT_Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWT_Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e30a8ca70fce46a78090d4a0c22209bcd973911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37a618a969b0045d7052713d0f328c89256effb")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWT_Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWT_Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
